bf154f0f59ef90fd01a522c642aab317b437b0b4
SomnathSwain <somnathswain1995@gmail.com>
2025-02-05 02:31:13 +0530

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "hitesh#google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "1111111"
+accountCity = "mumbai"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+
+*/
+
+console.table([ accountEmail, accountPassword, accountCity, accountState])
+
